578ad29780ee7762aa19e610c0e4e315a11beabd
Macallett <Macallett@users.noreply.github.com>
2014-04-02 11:45:37 +0100

    Update Start.java

--- src/macallett/mafiachickenfleets/start/Start.java
+++ src/macallett/mafiachickenfleets/start/Start.java
@@ -3,8 +3,7 @@ package macallett.mafiachickenfleets.start;
 public class Start {
 
 	public static void main(String[] args) {
-		System.out.println("MacBook Test");
-		System.out.println("Desktop sync test");
+		// not started yet :\
 	}
 
 }
